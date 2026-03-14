2dfb1f3896799962bf7e4153ba6ac56140880ca3
V-crasso <valery.crasso@gmail.com>
2026-03-14 12:52:10 +0100

    sasusu

---
+++ es1.java
@@ -0,0 +1,14 @@
+public class es1 {
+
+    public static void main(String[] args) {
+
+        int numero = 10;
+        double prezzo = 19.99;
+        String nome = "Mario";
+
+        System.out.println(numero);
+        System.out.println(prezzo);
+        System.out.println(nome);
+    }
+
+}
